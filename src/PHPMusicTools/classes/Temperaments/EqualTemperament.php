5b2fea47f689b9220bcf15a68d91afd37bad6f46
Ian Ring <109085+ianring@users.noreply.github.com>
2018-07-11 13:47:21 -0400

    Create EqualTemperament class

---
+++ src/PHPMusicTools/classes/Temperaments/EqualTemperament.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * EqualTemperament Class
+ *
+ * Inherits Temperament, dividing the scale into 12 equal parts of 100 cents
+ *
+ * @package      PHPMusicTools
+ * @author       Ian Ring <httpwebwitch@email.com>
+ */
+namespace ianring;
+require_once '../Temperament.php';
+class EqualTemperament extends Temperament {
+}
