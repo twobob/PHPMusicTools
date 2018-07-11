8bc728904a2da38366ec54da2072a7f8d7be01a8
Ian Ring <109085+ianring@users.noreply.github.com>
2018-07-11 13:56:37 -0400

    Create JustIntonation.php

---
+++ src/PHPMusicTools/classes/Temperaments/JustIntonation.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * JustIntonation Class
+ *
+ * @package      PHPMusicTools
+ * @author       Ian Ring <httpwebwitch@email.com>
+ */
+namespace ianring;
+require_once '../Temperament.php';
+class JustIntonation extends Temperament {
+}
