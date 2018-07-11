152c0d2289787ee2c2d2649ffb083ed0b80c3e7d
Ian Ring <109085+ianring@users.noreply.github.com>
2018-07-11 13:45:16 -0400

    Create a Temperament class

---
+++ src/PHPMusicTools/classes/Temperament.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Temperament Class
+ *
+ * Class for a tuning system, aka temperament. Perhaps this could translate Pitches into Frequencies
+ * but I doubt it's going to be that simple
+ *
+ * @package      PHPMusicTools
+ * @author       Ian Ring <httpwebwitch@email.com>
+ */
+namespace ianring;
+require_once 'PMTObject.php';
+class Temperament extends PMTObject {
+
+}
